a3466670c218f065ed969cbc8c2804cb677a9e84
minghaiyan <minghaiyan@sogou-inc.com>
2021-07-05 18:09:56 +0800

    增加服务启动停止脚本

--- start_tfserving.sh
+++ start_tfserving.sh
@@ -1,1 +1,2 @@
-docker run -p 8501:8501 -v /data/yangyu/MultiModel/:/models/MultiModel -t tensorflow/serving --model_config_file=/models/MultiModel/model.config
+mv ../tfserving /opt/app/
+docker run -itd -p 8501:8501 -v /opt/app/tfserving/:/models/MultiModel --name=tfserving -t tensorflow/serving --model_config_file=/models/MultiModel/model.config
