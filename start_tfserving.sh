6287713983511d5a94691a1501809a8995711499
minghaiyan <minghaiyan@sogou-inc.com>
2021-07-05 18:14:56 +0800

    更新脚本文件

--- start_tfserving.sh
+++ start_tfserving.sh
@@ -1,2 +1,4 @@
-mv ../tfserving /opt/app/
+if [ ! -d "/opt/app/tfserving" ]; then
+    mv ../tfserving /opt/app/
+fi
 docker run -itd -p 8501:8501 -v /opt/app/tfserving/:/models/MultiModel --name=tfserving -t tensorflow/serving --model_config_file=/models/MultiModel/model.config
