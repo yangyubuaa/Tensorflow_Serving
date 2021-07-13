375d48dd4f72b32501c124565f30b3067dc3470a
minghaiyan <minghaiyan@sogou-inc.com>
2021-07-13 17:15:36 +0800

    增加batch设置

--- start_tfserving.sh
+++ start_tfserving.sh
@@ -1,4 +1,4 @@
 if [ ! -d "/opt/app/tfserving" ]; then
     mv ../tfserving /opt/app/
 fi
-docker run -itd -p 8501:8501 -v /opt/app/tfserving/:/models/MultiModel --name=tfserving -t tensorflow/serving --model_config_file=/models/MultiModel/model.config
+docker run  -p 8501:8501 -v /opt/app/tfserving/:/models/MultiModel --name=tfserving -t tensorflow/serving --model_config_file=/models/MultiModel/model.config --enable_batching=true --batching_parameters_file=/models/MultiModel/batch.config
